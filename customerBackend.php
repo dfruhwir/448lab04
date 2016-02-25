7efbacc72323f7a4f17ab761bdeea32091835b43
Dillon Fruhwirth <d487f080@ku.edu>
2016-02-25 13:21:29 -0600

    Add welcome message with password

--- customerBackend.php
+++ customerBackend.php
@@ -1,3 +1,11 @@
 <?php
+	$u = $_POST["uname"];
+	$p = $_POST["pname"];
+	$gs6 = $_POST["s6"];
+	$i6s = $_POST["6s"];
+	$mg = $_POST["g"];
+	$b = $_POST["brick"];
 
+	echo "Hello " . $u . "! Thank you for shopping at We Cell Phones!<br>";
+	echo "Your password is " . $p . "<br>";
 ?>
